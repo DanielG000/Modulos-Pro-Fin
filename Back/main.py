c07156b499859872662d59286502d9a56005b96d
Daniel <danielcossio195@gmail.com>
2024-05-13 19:01:48 -0500

    Se ajuste puerto

--- Back/main.py
+++ Back/main.py
@@ -44,4 +44,4 @@ app.include_router(api_routes)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
