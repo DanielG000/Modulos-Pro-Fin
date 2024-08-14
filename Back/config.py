6ebaae038608a49e15c0f7da4faccb053b564613
DanielG000 <danielg.programmer@gmail.com>
2024-08-14 02:36:10 -0500

    Configuracion de base de datos usando variables de entorno.

--- Back/config.py
+++ Back/config.py
@@ -2,17 +2,22 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings
 
+from dotenv import load_dotenv
+import os
+
 top_dir = Path(__file__).resolve().parents[0]
 db_dir = top_dir / "db"
 db_name = "tesis.db"
 db_path = str(db_dir / db_name)
 
+load_dotenv()
+db_uri = os.getenv('DB_URI')
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Tesis backend"
     DESCRIPTION: str = "A FastAPI + SQLModel production-ready API"
     VERSION: str = "0.1"
-    DATABASE_URI: str = "postgresql://postgres:Univalle00@34.29.156.123:5432/postgres"
+    DATABASE_URI: str = db_uri
 
     class Config:
         case_sensitive = True
